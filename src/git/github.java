371bf8dd5a512543ba6b0676edad65d72a906f70
Rajitha-gree <Chavvarajitha5375@gmail.com>
2024-10-21 12:05:31 +0530

    changing java program

--- src/git/github.java
+++ src/git/github.java
@@ -3,8 +3,8 @@ package git;
 public class github {
 
 	public static void main(String[] args) {
-		String s = "Hello World";
-		System.out.println(s);
+		String s1 = "Hello World";
+		System.out.println(s1);
 
 	}
 
